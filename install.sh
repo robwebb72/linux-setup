d250fee817a75b86eec45e3f27142e69fb28eaaf
Rob <robstah72@gmail.com>
2024-10-19 12:49:33 +0100

    Add amiberry, fix sublime

--- install.sh
+++ install.sh
@@ -70,9 +70,9 @@ echo ">>>>> Installing GIMP"
 sudo apt install gimp -y
 
 echo ">>>>> Installing Sublime Text"
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
+sudo apt update -y
 sudo apt install sublime-text -y
 
 echo ">>>>> Installing Calibre"
@@ -126,6 +126,9 @@ flatpak install flathub com.github.eneshecan.WhatsAppForLinux -y
 
 echo ">>>>> Installing DropBox"
 flatpak install flathub com.dropbox.Client -y
+
+echo ">>>>> Installing Amiberry"
+flatpak install flathub com.blitterstudio.amiberry -y
 
 echo ">>>>> Installing Stacer"
 sudo add-apt-repository ppa:oguzhaninan/stacer
