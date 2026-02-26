1438260d75fe174761b456fa77de5f0b34a44a41
Rob <robstah72@gmail.com>
2026-02-26 18:23:27 +0000

    Fix Spotify repository URL in install script

--- install.sh
+++ install.sh
@@ -70,11 +70,9 @@ sudo apt update
 sudo apt install steam -y
 
 echo ">>>>> Installing Spotify"
-curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update -y && sudo apt-get install spotify-client -y
-
-
+curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg  
+echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
 
 echo ">>>>> Installing GIMP"
 sudo apt install gimp -y
